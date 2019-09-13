171434108395290a87ecbdba6a3accc8bd2b95f1
Iago Melo <iagosilvamelo@gmail.com>
2019-09-13 11:42:45 -0300

    Mostrando tela de login.

--- src/views/application/index.vue
+++ src/views/application/index.vue
@@ -1,14 +1,21 @@
 <template>
-	<div id="wrapper">
+	<div id="wrapper" v-if="auth == true">
         <leftMenu />
         <AppContent />
     </div>
+
+    <login v-else />
 </template>
 
 <script>
     import leftMenu   from "@/components/application/template/left-menu.vue";
     import AppContent from "@/components/application/template/content.vue";
-    export default { components: { leftMenu, AppContent } }
+    import login      from "./Login.vue";
+
+    export default { 
+        components: { leftMenu, AppContent, login },
+        computed: { auth() { return this.$store.state.auth } }
+    }
 </script>
 
 <style scoped>
